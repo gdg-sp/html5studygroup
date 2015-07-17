de114e42b48ffebf617bc4c6f94eb95f794656de
Rafael Antonio Lucio <rafaelantoniolucio@gmail>
2015-07-17 19:57:09 -0300

    atualizado express e suas dependencias

--- blogproject/server/server.js
+++ blogproject/server/server.js
@@ -1,42 +1,47 @@
-var http = require('http');
 var express = require('express');
-var cors = require('cors');
 var app = express();
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
 var jwt = require('express-jwt');
+var cors = require('cors');
 var dotenv = require('dotenv');
 
 dotenv.load();
 
 var authenticate = jwt({
-  secret: new Buffer(process.env.AUTH0_CLIENT_SECRET, 'base64'),
-  audience: process.env.AUTH0_CLIENT_ID
+    secret: new Buffer(process.env.AUTH0_CLIENT_SECRET, 'base64'),
+    audience: process.env.AUTH0_CLIENT_ID
 });
 
-
-app.configure(function () {
-
- // Request body parsing middleware should be above methodOverride
-  app.use(express.bodyParser());
-  app.use(express.urlencoded());
-  app.use(express.json());
-
-  app.use('/secured', authenticate);
-  app.use(cors());
-
-  app.use(app.router);
-});
-
-
-app.get('/ping', function(req, res) {
-  res.send(200, {text: "Resposta OK de método desprotegido!"});
-});
-
-app.get('/secured/ping', function(req, res) {
-  res.send(200, {text: "Resposta OK de método protegido, você está autenticado!"});
-})
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use(cors());
+app.use(methodOverride());
 
 var port = process.env.PORT || 4001;
 
-http.createServer(app).listen(port, function (err) {
-  console.log('Server iniciado em http://localhost:' + port);
+app.get('/', function(req, res) {
+    res.status(200).json({
+        message: 'welcome to our api!'
+    });
 });
+
+app.get('/ping', function(req, res) {
+    res.status(200).json({
+        text: "Resposta OK de método desprotegido!"
+    });
+});
+
+app.get('/secured/ping', function(req, res) {
+    res.status(200).json({
+        text: "Resposta OK de método protegido, você está autenticado!"
+    });
+});
+
+app.use('/secured', authenticate);
+
+app.listen(port);
+
+console.log('Running in localhost:' + port);
