6c58e694ae1d55ed7e78d389fe93c0f71d027b92
Rafael Antonio Lucio <rafaelantoniolucio@gmail>
2015-07-25 15:33:45 -0300

    incluído angular-translate no modulo login

--- blogproject/web/login/login.js
+++ blogproject/web/login/login.js
@@ -1,16 +1,36 @@
-angular.module( 'polymerblog.login', [
-  'auth0'
+angular.module('polymerblog.login', [
+  'auth0',
+  'pascalprecht.translate'
 ])
-.controller( 'LoginCtrl', function HomeController( $scope, auth, $location, store ) {
-
-  $scope.login = function() {
-    auth.signin({}, function(profile, token) {
-      store.set('profile', profile);
-      store.set('token', token);
-      $location.path("/");
-    }, function(error) {
-      console.log("Houve um erro ao logar, veja o console.", error);
+  .config(function ($translateProvider) {
+    $translateProvider.translations('pt-br', {
+      login: {
+        welcome: 'Bem vindo ao Polymer Blog, faça o login para continuar',
+        makeLogin: 'Fazer o login',
+        error: 'Houve erro ao logar veja o console.'
+      }
     });
-  }
+    $translateProvider.translations('en', {
+      login: {
+        welcome: 'Welcome to Polymer Blog login to continue.',
+        makeLogin: 'Make login',
+        error: 'There was an error log, see the console.'
+      }
+    });
 
-});
+    $translateProvider.useSanitizeValueStrategy(null);
+    $translateProvider.preferredLanguage('pt-br');
+  })
+  .controller('LoginCtrl', function HomeController($scope, auth, $location, store, $filter) {
+
+    $scope.login = function () {
+      auth.signin({}, function (profile, token) {
+        store.set('profile', profile);
+        store.set('token', token);
+        $location.path("/");
+      }, function (error) {
+        console.log($filter('translate')('login.error'), error);
+      });
+    }
+  });
+
