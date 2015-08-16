b696a8ed8a709a2edb20f5b190f06940ec129ca0
Rafael Antonio Lucio <rafaelantoniolucio@gmail>
2015-08-16 18:26:33 -0300

    adicionada internacionalização na página de login

--- blogproject/web/login/login.js
+++ blogproject/web/login/login.js
@@ -1,7 +1,25 @@
 angular.module( 'polymerblog.login', [
-  'auth0'
+  'auth0',
+  'pascalprecht.translate'
 ])
-.controller( 'LoginCtrl', function HomeController( $scope, auth, $location, store ) {
+.config(function ($translateProvider) {
+  $translateProvider.translations('pt-br', {
+    login: {
+      loginFail: 'Houve um erro ao logar, veja o console.',
+      wellcome: 'Bem vindo ao Polymer Blog, faça login para continuar.',
+      makeLogin: 'Fazer Login'
+    }
+  });
+
+  $translateProvider.translations('en', {
+    login: {
+      loginFail: 'There was error on login, see the console',
+      wellcome: 'Wellcome the Polymer Blog, make login to continue.',
+      makeLogin: 'Make Login'
+    }
+  });
+})
+.controller( 'LoginCtrl', function HomeController( $scope, auth, $location, store, $filter) {
 
   $scope.login = function() {
     auth.signin({}, function(profile, token) {
@@ -9,7 +27,7 @@ angular.module( 'polymerblog.login', [
       store.set('token', token);
       $location.path("/");
     }, function(error) {
-      console.log("Houve um erro ao logar, veja o console.", error);
+      console.log($filter('translate')('login.loginFail'), error);
     });
   }
 
